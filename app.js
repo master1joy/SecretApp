2db6a45498c4fcde0da387ed88f580206c76b858
joydip <pauljoydip92@gmail.com>
2023-05-13 13:46:55 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,59 @@
+//jshint esversion:6
+require('dotenv').config();
+const express = require('express')
+const bodyParser = require('body-parser')
+const ejs = require('ejs')
+const mongoose= require('mongoose')
+const encrypt = require('mongoose-encryption')
+
+const app = express()
+
+app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect("mongodb://127.0.0.1:27017/userDB");
+const userSchema=new mongoose.Schema({
+  email:String,
+  password:String
+});
+
+
+userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"]});
+
+const User = mongoose.model('user', userSchema);
+app.get('/', function(req, res){
+  res.render("home");
+});
+
+app.get('/login', function(req, res){
+  res.render("login");
+});
+
+app.get('/register', function(req, res){
+  res.render("register");
+});
+
+app.post('/register', function(req, res){
+  const newUser = new User({
+    email:req.body.username,
+    password:req.body.password
+  });
+  newUser.save().then(
+    res.render("secrets")
+  );
+});
+
+app.post("/login", function(req, res){
+  User.findOne({
+    email:req.body.username
+  }).then((founduser)=>{
+    res.render("secrets")
+  });
+})
+
+app.listen(3000, function(){
+  console.log("server is started listening on 3000");
+})
