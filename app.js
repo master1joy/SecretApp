eec6717ff1ed1cdd7311e1e566d436eaf5c271a8
joydip <pauljoydip92@gmail.com>
2023-05-16 19:08:50 +0530

    improve security

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const ejs = require('ejs')
 const mongoose= require('mongoose')
-const encrypt = require('mongoose-encryption')
+const md5 = require('md5')
 
 const app = express()
 
@@ -19,9 +19,6 @@ const userSchema=new mongoose.Schema({
   email:String,
   password:String
 });
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET_KEY, encryptedFields: ["password"]});
 
 const User = mongoose.model('user', userSchema);
 app.get('/', function(req, res){
@@ -39,7 +36,7 @@ app.get('/register', function(req, res){
 app.post('/register', function(req, res){
   const newUser = new User({
     email:req.body.username,
-    password:req.body.password
+    password:md5(req.body.password)
   });
   newUser.save().then(
     res.render("secrets")
@@ -47,10 +44,16 @@ app.post('/register', function(req, res){
 });
 
 app.post("/login", function(req, res){
+  const username = req.body.username;
+  const password = md5(req.body.password);
   User.findOne({
-    email:req.body.username
-  }).then((founduser)=>{
-    res.render("secrets")
+    email:username,
+  },'password').then((founduser)=>{
+    if (password === founduser.password) {
+        res.render("secrets")
+    }else {
+      res.redirect("login")
+    }
   });
 })
 
